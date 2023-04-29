e8eb5b94e6073a54a01bc76752994445cbce4ca5
Neil Nie <yongyang.nie@gmail.com>
2023-04-28 23:05:34 -0400

    the empty classifier is mostly converging

--- model/convnet.py
+++ model/convnet.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 
 def build_model(out):
     model = models.Sequential()
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(100, 100, 3)))
-    model.add(layers.MaxPooling2D((2, 2)))
-    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(16, (3, 3), activation='relu', input_shape=(100, 100, 3)))
+    model.add(layers.AveragePooling2D((2, 2)))
+    model.add(layers.Conv2D(32, (3, 3), activation='relu'))
+    model.add(layers.AveragePooling2D((2, 2)))
     
     model.add(layers.Flatten())
     model.add(layers.Dropout(0.5))
--- train_model.py
+++ train_model.py
@@ -4,33 +4,36 @@ import matplotlib.pyplot as plt
 from model.convnet import build_model
 
 
-data_dir = "./dataset"
+data_dir = "./dataset_2"
 # dataset = tf.keras.preprocessing.image_dataset_from_directory(
 #     data_dir, batch_size=64
 # )
 
 img_gen = tf.keras.preprocessing.image.ImageDataGenerator(
     rescale=1.0 / 255,
-    rotation_range=20,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
+    rotation_range=0,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
     vertical_flip=False,
-    horizontal_flip=True
+    horizontal_flip=True,
+    validation_split=0.2
 )
 
-img_data = img_gen.flow_from_directory(data_dir, target_size=(100, 100), batch_size=8)
+img_data = img_gen.flow_from_directory(data_dir, target_size=(100, 100), batch_size=8, subset="training")
+test_data = img_gen.flow_from_directory(data_dir, target_size=(100, 100), batch_size=8, subset="validation")
+
 print(len(img_data))
 
-model = build_model(4)
+model = build_model(2)
 model.summary()
 
-
-model.compile(optimizer=optimizers.Adam(learning_rate = 0.00001),
-              loss=tf.keras.losses.CategoricalCrossentropy(from_logits=False),
+model.compile(optimizer=optimizers.Adam(learning_rate=0.000025),
+              loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-history = model.fit(img_data, epochs=5)
+history = model.fit(img_data, epochs=6, validation_data=test_data)
 
+model.save("empty_detector")
 print(history)
 # plt.plot(history['accuracy'])
 # plt.show()
