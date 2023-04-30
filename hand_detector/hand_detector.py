09b3f226ba890fc9f3276bc0c6fe2cf0a9b4019d
Neil Nie <yongyang.nie@gmail.com>
2023-04-30 17:09:04 -0400

    fix hand detector bug

--- hand_detector/hand_detector.py
+++ hand_detector/hand_detector.py
@@ -1,14 +1,16 @@
+import numpy as np
 import tensorflow as tf
 import tf.keras as keras
 from PIL import Image
 
-class hand_detector:
-    def __init__(model_path, input_size=(160, 160)):
+
+class HandDetector:
+
+    def __init__(self, model_path, input_size=(160, 160)):
         self.model = keras.models.load_model(model_path)
         self.input_size = input_size
 
-    
-    def predict(img_arr):
+    def predict(self, img_arr):
         # first resize
         img = Image.fromarray(img_arr.astype('uint8'), 'RGB')
 
@@ -16,7 +18,7 @@ class hand_detector:
 
         dataset = tf.data.Dataset.from_tensor_slices([np.array(img)])
 
-        predict = model.predict(testing_dataset.batch(1))[0, 0]
+        predict = self.model.predict(testing_dataset.batch(1))[0, 0]
 
         return int(predict > 0)
 
