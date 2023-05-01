c4f690f7b93a25ad102a357b68d0ebcbf63dae6e
Neil Nie <yongyang.nie@gmail.com>
2023-05-01 13:39:48 -0400

    fix model shape and activation bug

--- model/convnet.py
+++ model/convnet.py
@@ -3,9 +3,9 @@ from tensorflow.keras import datasets, layers, models
 import matplotlib.pyplot as plt
 
 
-def build_model(out):
+def build_piece_classification_model(out):
     model = models.Sequential()
-    model.add(layers.Conv2D(16, (3, 3), activation='relu', input_shape=(100, 100, 3)))
+    model.add(layers.Conv2D(16, (3, 3), activation='relu', input_shape=(100, 100, 1)))
     model.add(layers.AveragePooling2D((2, 2)))
     model.add(layers.Conv2D(32, (3, 3), activation='relu'))
     model.add(layers.AveragePooling2D((2, 2)))
@@ -13,6 +13,6 @@ def build_model(out):
     model.add(layers.Flatten())
     model.add(layers.Dropout(0.5))
     model.add(layers.Dense(64, activation='relu'))
-    model.add(layers.Dense(out))
+    model.add(layers.Dense(out, activation="softmax"))
     return model
     
